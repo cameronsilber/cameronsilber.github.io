44e541d21033044017368f5970038a4c56ab81d5
Cameron Silber <camysilber7@gmail.com>
2016-05-10 08:26:07 -0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/flyguy1120/pen/GZPGqb.
+
+ 
---
+++ js/index.js
@@ -0,0 +1,15 @@
+/*
+Required elements of the project:
+-Lab report - normal lab report structure 
+Weight: 15/15
+Introduction, procedure, materials, data, analysis, conclusion
+Documentation of experiment (pictures, video, diagram)
+A graph that shows a relationship between the variables collected
+An equation that describes the physical system
+Give Landgreen a printed copy.
+
+-Very Short Presentations  under 5 min
+Weight 5/5
+ppt, prezi, other
+Documentation of experiment (pictures, video, data)
+*/
